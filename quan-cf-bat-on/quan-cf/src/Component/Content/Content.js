b23b25da957d64a17afdd33f18149660e1977d01
= <hoangcfqq@gmail.com>
2020-05-19 20:52:38 +0700

    bt

--- quan-cf-bat-on/quan-cf/src/Component/Content/Content.js
+++ quan-cf-bat-on/quan-cf/src/Component/Content/Content.js
@@ -58,70 +58,69 @@ class Content extends Component {
         // console.log(this.state.data)
         const {data} = this.state;
         return (
-            <div> 
-            {/* phần header */}
-            {/* <!-- slide --> */}
-            <div id="myCarousel" className="carousel slide border" data-ride="carousel">
-                <div className="carousel-inner">
-                    <div className="carousel-item">
-                    <img className="d-block w-100" src="./img/cf4.jpg" alt="Lion" />
-                    <div className="carousel-caption d-none d-sm-block">
-                        <Link className="btn btn-primary btn-xl rounded-pill mt-5" to='/OrderList'>ORDER LIST</Link>
-                        <hr/>
-                        <h5>Mỗi giọt cafe rơi, thời gian như ngừng lại.</h5>
-                        <small>
-                        Nếu đây là cà phê, hãy mang cho tôi một ít trà; nhưng nếu đây là trà, hãy mang cho tôi một ít cà phê.
-                        </small>
-                    </div>
-                    </div>
-                    <div className="carousel-item active">
-                    <img className="d-block w-100" src="./img/cf7.jpg" alt="Leopard" />
-                    <div className="carousel-caption d-none d-sm-block">
-                        <Link className="btn btn-primary btn-xl rounded-pill mt-5" to='/OrderList'>ORDER LIST</Link>
-                        <hr/>
-                        <h5>Love Cafe</h5>
-                        <small>
-                        Good morning – ngày mới vui vẻ bên tách cafe thơm phức
-                        </small>
-                    </div>
-                    </div>
-                    <div className="carousel-item">
-                    <img className="d-block w-100" src="./img/cf1.jpg" alt="Cat" />
-                    <div className="carousel-caption d-none d-sm-block">
-                        <Link className="btn btn-primary btn-xl rounded-pill mt-5" to='/OrderList'>ORDER LIST</Link>
-                        <hr/>
-                        <h5>Black coffee</h5>
-                        <small>
-                        Đắng cafe, đắng không lời. Đắng câu duyên lỡ, đắng đời nhau ra.
-                        </small>
-                    </div>
-                    </div>
-                    <div className="carousel-item">
-                    <img className="d-block w-100" src="./img/cf6.jpg" alt="Lion" />
-                    <div className="carousel-caption d-none d-sm-block">
-                        <Link className="btn btn-primary btn-xl rounded-pill mt-5" to='/OrderList'>ORDER LIST</Link>
-                        <hr/>
-                        <h5>Cafe chồn</h5>
-                        <small>
-                        Chỉ những người yêu cà phê đen đích thực, mới nhấm nháp được một chút ngọt ngào sau cái đắng.
-                        </small>
-                    </div>
-                    </div>
+    <div> 
+        {/* phần header */}
+        {/* <!-- slide --> */}
+        <div id="myCarousel" className="carousel slide border" data-ride="carousel">
+            <div className="carousel-inner">
+                <div className="carousel-item">
+                <img className="d-block w-100" src="./img/cf4.jpg" alt="Lion" />
+                <div className="carousel-caption d-none d-sm-block">
+                    <Link className="btn btn-primary btn-xl rounded-pill mt-5" to='/OrderList'>ORDER LIST</Link>
+                    <hr/>
+                    <h5>Mỗi giọt cafe rơi, thời gian như ngừng lại.</h5>
+                    <small>
+                    Nếu đây là cà phê, hãy mang cho tôi một ít trà; nhưng nếu đây là trà, hãy mang cho tôi một ít cà phê.
+                    </small>
                 </div>
-                <a className="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
-                    <span className="carousel-control-prev-icon" aria-hidden="true" />
-                    <span className="sr-only">Previous</span>
-                </a>
-                <a className="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
-                    <span className="carousel-control-next-icon" aria-hidden="true" />
-                    <span className="sr-only">Next</span>
-                </a>
+                </div>
+                <div className="carousel-item active">
+                <img className="d-block w-100" src="./img/cf7.jpg" alt="Leopard" />
+                <div className="carousel-caption d-none d-sm-block">
+                    <Link className="btn btn-primary btn-xl rounded-pill mt-5" to='/OrderList'>ORDER LIST</Link>
+                    <hr/>
+                    <h5>Love Cafe</h5>
+                    <small>
+                    Good morning – ngày mới vui vẻ bên tách cafe thơm phức
+                    </small>
+                </div>
+                </div>
+                <div className="carousel-item">
+                <img className="d-block w-100" src="./img/cf1.jpg" alt="Cat" />
+                <div className="carousel-caption d-none d-sm-block">
+                    <Link className="btn btn-primary btn-xl rounded-pill mt-5" to='/OrderList'>ORDER LIST</Link>
+                    <hr/>
+                    <h5>Black coffee</h5>
+                    <small>
+                    Đắng cafe, đắng không lời. Đắng câu duyên lỡ, đắng đời nhau ra.
+                    </small>
+                </div>
+                </div>
+                <div className="carousel-item">
+                <img className="d-block w-100" src="./img/cf6.jpg" alt="Lion" />
+                <div className="carousel-caption d-none d-sm-block">
+                    <Link className="btn btn-primary btn-xl rounded-pill mt-5" to='/OrderList'>ORDER LIST</Link>
+                    <hr/>
+                    <h5>Cafe chồn</h5>
+                    <small>
+                    Chỉ những người yêu cà phê đen đích thực, mới nhấm nháp được một chút ngọt ngào sau cái đắng.
+                    </small>
+                </div>
+                </div>
             </div>
-
+            <a className="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true" />
+                <span className="sr-only">Previous</span>
+            </a>
+            <a className="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true" />
+                <span className="sr-only">Next</span>
+            </a>
+        </div>
         {/* <!-- end lide --> */}
-            {/* end phần header */}
-            {/* phần content */}
-            <div className="container-buld">
+        {/* end phần header */}
+        {/* phần content */}
+        <div className="container-buld">
             <div className="container content">
                 <div className="row">
                     <div className="col-6 col-sm-12 col-md-12">
@@ -153,41 +152,38 @@ class Content extends Component {
                         </table>
                     </div>
                 </div>
-                {/* //////   */}
-                <h1 className="Danh_sach_ban">Danh sách bàn</h1>
+                    {/* //////   */}
+                    <h1 className="Danh_sach_ban">Danh sách bàn</h1>
                 <div className="row">
-                <div className="col-6 col-sm-12 col-md-12">
-                    <table className="table">
-                        <thead className="bang_user">
-                                <tr>
-                                    <th>Bàn</th>
-                                    <th>Đồ uống</th>
-                                    <th>Số lượng</th>
-                                </tr>
-                            </thead>
-                        <tbody>
-                            {/* a6/11 Hàm map giống như vòng lặp. Mỗi lần lặp là truyền nguyên một phần tử (item)
-                            của mảng vào hàm map...
-                            */}
-                            {data.map((item,index)=>{
-                                // console.log(item)
-                                return (
-
-                                <tr>
-                                    {/* b1:  */}
-                                    <Table item={item}/>
-                                </tr>
-                                )
-                            })}
-                            
-                        </tbody>
-                    </table>
+                    <div className="col-6 col-sm-12 col-md-12">
+                        <table className="table">
+                            <thead className="bang_user">
+                                    <tr>
+                                        <th>Bàn</th>
+                                        <th>Đồ uống</th>
+                                        <th>Số lượng</th>
+                                    </tr>
+                                </thead>
+                            <tbody>
+                                {/* a6/11 Hàm map giống như vòng lặp. Mỗi lần lặp là truyền nguyên một phần tử (item)
+                                của mảng vào hàm map...
+                                */}
+                                {data.map((item,index)=>{
+                                    return (
+                                    <tr>
+                                        {/* b1:  */}
+                                        <Table item={item}/>
+                                    </tr>
+                                    )
+                                })}
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
-        </div>
         {/* end phần content */}
-        </div>
+    </div>
         )
     }
 }
